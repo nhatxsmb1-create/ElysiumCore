0b1fed6fc8ed6340b322927c1af3d20eba727c7c
nhatxsmb1-create <nhatxsmb1@gmail.com>
2026-07-27 18:54:09 +0700

    Update ElysiumCore.java

--- src/main/java/dev/elysium/core/ElysiumCore.java
+++ src/main/java/dev/elysium/core/ElysiumCore.java
@@ -1,13 +1,16 @@
 package dev.elysium.core;
 
+import dev.elysium.core.achievement.AchievementManager;
 import dev.elysium.core.api.CoreAPI;
 import dev.elysium.core.config.ConfigManager;
 import dev.elysium.core.database.DatabaseManager;
 import dev.elysium.core.database.MySQLDatabase;
 import dev.elysium.core.database.SQLiteDatabase;
 import dev.elysium.core.gui.GuiManager;
+import dev.elysium.core.leaderboard.LeaderboardManager;
 import dev.elysium.core.placeholder.ElysiumExpansion;
 import dev.elysium.core.player.PlayerManager;
+import dev.elysium.core.season.SeasonManager;
 import dev.elysium.core.util.CooldownManager;
 import dev.elysium.core.util.Scheduler;
 import net.milkbowl.vault.economy.Economy;
@@ -19,13 +22,16 @@ public class ElysiumCore extends JavaPlugin {
 
     private static ElysiumCore instance;
 
-    private ConfigManager  configManager;
-    private DatabaseManager databaseManager;
-    private PlayerManager  playerManager;
-    private GuiManager     guiManager;
-    private CooldownManager cooldownManager;
-    private Scheduler      scheduler;
-    private Economy        economy;
+    private ConfigManager      configManager;
+    private DatabaseManager    databaseManager;
+    private PlayerManager      playerManager;
+    private GuiManager         guiManager;
+    private CooldownManager    cooldownManager;
+    private Scheduler          scheduler;
+    private AchievementManager achievementManager;
+    private LeaderboardManager leaderboardManager;
+    private SeasonManager      seasonManager;
+    private Economy            economy;
 
     @Override
     public void onEnable() {
@@ -36,10 +42,13 @@ public class ElysiumCore extends JavaPlugin {
 
         setupDatabase();
 
-        playerManager   = new PlayerManager(this);
-        guiManager      = new GuiManager(this);
-        cooldownManager = new CooldownManager();
-        scheduler       = new Scheduler(this);
+        playerManager      = new PlayerManager(this);
+        guiManager         = new GuiManager(this);
+        cooldownManager    = new CooldownManager();
+        scheduler          = new Scheduler(this);
+        achievementManager = new AchievementManager(this);
+        leaderboardManager = new LeaderboardManager(this);
+        seasonManager      = new SeasonManager(this);
 
         CoreAPI.init(this);
 
@@ -52,7 +61,6 @@ public class ElysiumCore extends JavaPlugin {
 
         Bukkit.getPluginManager().registerEvents(playerManager, this);
         Bukkit.getPluginManager().registerEvents(guiManager, this);
-
         getCommand("elysium").setExecutor(new ElysiumCommand(this));
 
         startAutoSave();
@@ -70,23 +78,18 @@ public class ElysiumCore extends JavaPlugin {
 
     private void setupDatabase() {
         String type = getConfig().getString("database.type", "sqlite");
-        databaseManager = "mysql".equalsIgnoreCase(type)
-            ? new MySQLDatabase(this) : new SQLiteDatabase(this);
+        databaseManager = "mysql".equalsIgnoreCase(type) ? new MySQLDatabase(this) : new SQLiteDatabase(this);
         databaseManager.initialize();
     }
 
     private void setupVault() {
-        if (getServer().getPluginManager().getPlugin("Vault") == null) {
-            getLogger().warning("Vault not found!"); return;
-        }
-        RegisteredServiceProvider<Economy> rsp =
-            getServer().getServicesManager().getRegistration(Economy.class);
+        if (getServer().getPluginManager().getPlugin("Vault") == null) { getLogger().warning("Vault not found!"); return; }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
         if (rsp == null) { getLogger().warning("No economy provider!"); return; }
         economy = rsp.getProvider();
         getLogger().info("Vault hooked: " + economy.getName());
     }
 
-    /** Auto-save tat ca player online moi 5 phut */
     private void startAutoSave() {
         if (!getConfig().getBoolean("auto-save.enabled", true)) return;
         int interval = getConfig().getInt("auto-save.interval", 6000);
@@ -99,12 +102,15 @@ public class ElysiumCore extends JavaPlugin {
         }, interval, interval);
     }
 
-    public static ElysiumCore getInstance()     { return instance; }
-    public ConfigManager   getConfigManager()   { return configManager; }
-    public DatabaseManager getDatabaseManager() { return databaseManager; }
-    public PlayerManager   getPlayerManager()   { return playerManager; }
-    public GuiManager      getGuiManager()      { return guiManager; }
-    public CooldownManager getCooldownManager() { return cooldownManager; }
-    public Scheduler       getScheduler()       { return scheduler; }
-    public Economy         getEconomy()         { return economy; }
-}
+    public static ElysiumCore getInstance()          { return instance; }
+    public ConfigManager   getConfigManager()        { return configManager; }
+    public DatabaseManager getDatabaseManager()      { return databaseManager; }
+    public PlayerManager   getPlayerManager()        { return playerManager; }
+    public GuiManager      getGuiManager()           { return guiManager; }
+    public CooldownManager getCooldownManager()      { return cooldownManager; }
+    public Scheduler       getScheduler()            { return scheduler; }
+    public AchievementManager getAchievementManager(){ return achievementManager; }
+    public LeaderboardManager getLeaderboardManager(){ return leaderboardManager; }
+    public SeasonManager   getSeasonManager()        { return seasonManager; }
+    public Economy         getEconomy()              { return economy; }
+                                }
